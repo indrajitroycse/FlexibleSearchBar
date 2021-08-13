ea1e04600a98d8168495df528d511c7b43c79200
veejay <veejaynext@gmail.com>
2021-08-13 12:21:56 +0530

    added LICENSE file and copyright info

--- flexiblesearchbarview/src/main/java/com/yuqirong/flexiblesearchbarview/Utils.java
+++ flexiblesearchbarview/src/main/java/com/yuqirong/flexiblesearchbarview/Utils.java
@@ -1,3 +1,19 @@
+/*
+2 * Copyright (C) 2020-21 Application Library Enigineering Group
+3 *
+4 * Licensed under the Apache License, Version 2.0 (the "License");
+5 * you may not use this file except in compliance with the License.
+6 * You may obtain a copy of the License at
+7 *
+8 *      http://www.apache.org/licenses/LICENSE-2.0
+9 *
+10 * Unless required by applicable law or agreed to in writing, software
+11 * distributed under the License is distributed on an "AS IS" BASIS,
+12 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+13 * See the License for the specific language governing permissions and
+14 * limitations under the License.
+15 */
+
 package com.yuqirong.flexiblesearchbarview;
 
 import ohos.global.resource.Resource;
